a0f98dfc58cb1aea627523ec94424ce7fd47a99b
aherod <aherod@thunderhead.com>
2019-07-05 15:49:24 -0400

    changes to runtime options selection

--- locust/static/runtimeOptions.js
+++ locust/static/runtimeOptions.js
@@ -2,7 +2,9 @@ function displayRuntimeOptions() {
     var drop_down = document.getElementById("runtime_options")
     if (drop_down.options[drop_down.selectedIndex].value == "define_runtime") {
         document.getElementById("runtime").style.display = "block";
+        document.getElementById("runtime_label").style.display = "block";
     } else {
         document.getElementById("runtime").style.display = "none";
+        document.getElementById("runtime_label").style.display = "none";
     }
 }
